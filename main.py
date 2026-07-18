8fc0f6ffdd6c6818ad668f97d0b13bae1f5435d6
vodoleikin <vodoleikin@yandex.ru>
2026-07-18 20:26:17 +0300

    Added main.py-file

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+
+size = [400, 600]
+
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Snake_game")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
